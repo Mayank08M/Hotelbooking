a3d8e436e03c10df5f47860c379270b10d389ccc
Mayank <mayank0806md@gmail.com>
2023-12-06 12:25:06 +0530

    Added a contact form in contact page

--- pages/contact.js
+++ pages/contact.js
@@ -1,9 +1,60 @@
 import React from 'react'
-import styles from '../styles/Home.module.css'
+import Contactstyles from '../styles/Contact.module.css'
 
 const contact = () => {
   return (
-    <div className={styles.container}>Pick the best hotels for you.</div>
+    <>
+    <div className={Contactstyles.body}>
+    <div className={Contactstyles.upbody}>
+    <div className={Contactstyles.container}>
+      <div className={Contactstyles.card}>
+        <h2>Contact Us</h2>
+      <div className={Contactstyles.row}>
+    <div className={Contactstyles.col}>
+      <div className={Contactstyles.formgroup}>
+        <label>First Name</label>
+        <input type="text"/>
+      </div>
+    </div>
+
+    <div className={Contactstyles.col}>
+      <div className={Contactstyles.formgroup}>
+        <label>Surname</label>
+        <input type="text"/>
+      </div>
+    </div>
+
+    <div className={Contactstyles.col}>
+      <div className={Contactstyles.formgroup}>
+        <label>Email</label>
+        <input type="text"/>
+      </div>
+    </div>
+
+    <div className={Contactstyles.col}>
+      <div className={Contactstyles.formgroup}>
+        <label>Phone</label>
+        <input type="text"/>
+      </div>
+    </div>
+
+    <div className={Contactstyles.col}>
+      <div className={Contactstyles.formgroup}>
+        <label>Message</label>
+        <textarea></textarea>
+      </div>
+    </div>
+
+    <div className={Contactstyles.col}>
+      <button type="submit" value="Submit">Submit</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+</div>
+    
+    </>
   )
 }                                                                       
 
