883dbee399e065dd6ca412d9bf7b883eca130959
Mayank <mayank0806md@gmail.com>
2023-12-12 16:06:22 +0530

    Modified 404 page

--- pages/404.js
+++ pages/404.js
@@ -1,5 +1,5 @@
 import styles from '../styles/Home.module.css'
 
 export default function Custom404() {
-    return <h1 className={styles.container}>404 - Page Not Found</h1>
+    return <h1 className={styles.eror}>404 - Page Not Found</h1>
   }
